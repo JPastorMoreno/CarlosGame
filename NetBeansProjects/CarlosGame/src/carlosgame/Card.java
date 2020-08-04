57f19546f5d2278f0bc6a1522e9ab6c5a90e9c86
JPastorMoreno <47524605+JPastorMoreno@users.noreply.github.com>
2020-08-04 23:12:20 +0200

    pruebaDelJuego

---
+++ NetBeansProjects/CarlosGame/src/carlosgame/Card.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carlosgame;
+
+/**
+ *
+ * @author JAWFTD
+ */
+public class Card {
+    int number;
+    String verb;
+    String color;
+
+    public Card(int number,String color,String verb) {
+     this.number = number;
+     this.color = color;
+     this.verb = verb;
+    }
+    
+    
+}
